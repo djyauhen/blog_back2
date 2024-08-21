9783baa313fa23aa4a92e743f1b7a4a53ec134af
Djyauhen <fursaevgenij@gmail.com>
2024-08-21 23:01:28 +0300

    Dev

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const config = {
     // secret: '9238fSf9fAKckj332Knaksnf9012ADSN',
     port: 443,
     // url: `http://77.222.38.178:8080/`,
-    host: '192.168.1.6',
+    host: '185.244.50.147',
     db: {
         dbUrl: 'mongodb+srv://admin:Pass123@svetlanabd.acgtgzb.mongodb.net/?appName=SvetlanaBD',
         dbName: 'blog'
