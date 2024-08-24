9ae0242f9828034ba75193c7a25a32c56db9d664
Djyauhen <fursaevgenij@gmail.com>
2024-08-24 09:25:34 +0300

    cmd

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
     // secret: '9238fSf9fAKckj332Knaksnf9012ADSN',
-    port: 443,
+    port: 80,
     host: `77.222.38.178`,
     // host: '185.244.50.147',
-    url: `http://77.222.38.178:443/`,
+    url: `http://advokat-degtyareva.ru/`,
     db: {
         dbUrl: 'mongodb+srv://admin:Pass123@svetlanabd.acgtgzb.mongodb.net/?appName=SvetlanaBD',
         dbName: 'blog'
